ad7408b2af11766a38385b54879eafd8823576df
Władysław Sakolski <wsakolski@gmail.com>
2020-09-24 22:19:43 +0200

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  const name = (params: any) => {
-    console.log('blablabla')
-    console.log('')
-  }
   return (
     <div className="App">
       <header className="App-header">
